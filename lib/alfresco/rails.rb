f7b795f9e92ed7647d5c04fe4c8a6ac197fb9875
Igor Rocha <igortice@gmail.com>
2020-04-25 21:07:48 -0300

    first commit

---
+++ lib/alfresco/rails.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'alfresco/rails/version'
+
+module Alfresco
+  module Rails
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/alfresco/rails_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Alfresco::Rails do
+  it 'has a version number' do
+    expect(Alfresco::Rails::VERSION).not_to be nil
+  end
+end
