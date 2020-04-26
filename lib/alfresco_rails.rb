31b011bc8318976585cac1cef31135587737eb54
Igor Rocha <igortice@gmail.com>
2020-04-25 21:22:41 -0300

    remove idea

--- lib/alfresco_rails.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require 'alfresco_rails/version'
-
-module AlfrescoRails
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- spec/alfresco_rails/alfresco_rails_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe AlfrescoRails do
-  it 'has a version number' do
-    expect(AlfrescoRails::VERSION).not_to be nil
-  end
-end
